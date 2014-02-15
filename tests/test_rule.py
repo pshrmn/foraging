ad381ace7356918d2de6fdc2ab93c1f18f6a93d7
Paul Sherman <paul.sherman.88@gmail.com>
2014-02-14 18:52:19 -0600

    Rule.index tests

--- tests/test_rule.py
+++ tests/test_rule.py
@@ -6,7 +6,7 @@ from lxml.etree import fromstring
 class RuleTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.good_rules = {"name": "links", "selector": "a", "capture": "attr-href"}
+        self.good_rules = {"name": "links", "selector": "a", "capture": "attr-href", "index": ""}
         self.html = fromstring('''<div>
             <a href="#">Testing<span> more testing</span></a>
             <a href="http://www.example.com">Second</a>
@@ -32,7 +32,30 @@ class RuleTestCase(unittest.TestCase):
         captured_attrs = r.get(self.html)
         self.assertEqual(captured_attrs, ["#", "http://www.example.com"])
 
+    def test_index(self):
+        # no index
+        r = rule.Rule(self.good_rules)
+        r = rule.Rule(self.good_rules)
+        self.assertEqual(r.index, None)
+        captured_attrs = r.get(self.html)
+        self.assertEqual(len(captured_attrs), 2)
+        self.assertEqual(captured_attrs, ["#", "http://www.example.com"])
 
+        # positive index
+        self.good_rules["index"] = 1
+        r = rule.Rule(self.good_rules)
+        self.assertEqual(r.index, 1)
+        captured_attrs = r.get(self.html)
+        self.assertEqual(len(captured_attrs), 1)
+        self.assertEqual(captured_attrs[0], "http://www.example.com")
+
+        # negative index
+        self.good_rules["index"] = -1
+        r = rule.Rule(self.good_rules)
+        self.assertEqual(r.index, -1)
+        captured_attrs = r.get(self.html)
+        self.assertEqual(len(captured_attrs), 1)
+        self.assertEqual(captured_attrs[0], "#")
 
 if __name__=="__main__":
     unittest.main()
