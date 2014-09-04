960a93cc39898581c36efead3669e77d054aedb3
Paul Sherman <paul.sherman.88@gmail.com>
2014-09-03 23:44:11 -0500

    slight server.py cleanup

--- collector/web/server.py
+++ collector/web/server.py
@@ -4,18 +4,25 @@ import os
 import argparse
 from urlparse import urlparse
 
-DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-SITE_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rules')
-
-if not os.path.exists(SITE_DIRECTORY):
-    os.mkdir(SITE_DIRECTORY)
-
 app = Flask(__name__)
 
+def set_directory(name):
+    global SITE_DIRECTORY
+    SITE_DIRECTORY = name
+    if not os.path.exists(SITE_DIRECTORY):
+        os.mkdir(SITE_DIRECTORY)
+
 def underscore_host(host):
+    """
+    replace periods in a url with underscores for valid folder name
+    """
     return host.replace(".", "_")
 
 def host_folder(host):
+    """
+    determine directory name based on hostname
+    create directory if it doesn't already exist
+    """
     dir_name = os.path.join(SITE_DIRECTORY, underscore_host(host))
     if not os.path.isdir(dir_name):
         os.mkdir(dir_name)
@@ -43,11 +50,14 @@ def upload():
         json.dump(data["group"], fp, indent=2)
     return jsonify({"error": False})
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Server to run with collect-chrome')
+    parser = argparse.ArgumentParser(description='Server to run with collectorjs')
     parser.add_argument('--folder', '-F', dest='directory',
                    help='folder to save groups to')
     args = parser.parse_args()    
-    SITE_DIRECTORY = args.directory or SITE_DIRECTORY
+
+    directory = args.directory or os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rules')
+    set_directory(directory)
     app.run(debug=True)
+else:
+    set_directory(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rules'))
