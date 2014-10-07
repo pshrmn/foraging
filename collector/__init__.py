b0b9d6c9eb9aaf77f5f33a1c332e142dc9140d05
Paul Sherman <paul.sherman.88@gmail.com>
2014-10-07 00:23:59 -0500

    typo fix and version bump

--- collector/__init__.py
+++ collector/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.6.3"
+__version__ = "0.6.4"
 from .crawl import Schema
 from .crawl.shortcuts import crawl_url, crawl_schema
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ packages = find_packages()
 
 setup(
     name = "Collector",
-    version = "0.6.3",
+    version = "0.6.4",
     packages = packages,
     install_requires = ["Flask", "cssselect>=0.9.1", "requests>=2", "lxml>=3.3.1", "selenium>=2.43.0"],
     author = "Paul Sherman",
     author_email = "paul.sherman.88@gmail.com",
     url = "https://github.com/psherman/collector",
-    download_url = "https://githbu.com/psherman/collector/tarball/0.6.3"
+    download_url = "https://github.com/psherman/collector/tarball/0.6.4"
 )
