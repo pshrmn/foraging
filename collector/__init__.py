15d241a4e2ae91e26e0bb4daab9316b39b06bf4c
Paul Sherman <paul.sherman.88@gmail.com>
2015-03-15 13:59:13 -0500

    fetch.Cache class

--- collector/__init__.py
+++ collector/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.9.5"
+__version__ = "0.9.6"
 
 from .crawl import get_url, Schema, SimpleSchema
--- collector/crawl/fetch.py
+++ collector/crawl/fetch.py
@@ -3,6 +3,7 @@ from lxml import html
 import time
 import glob
 import os
+import errno
 try:
     from urllib.parse import urlparse
 except ImportError:
@@ -21,59 +22,80 @@ def url_info(url):
     return domain, filename
 
 
-class Fetch(object):
-    def __init__(self, sleep_time=5, cache_folder=None):
-        self.last = None
-        self.sleep_time = sleep_time
-        self.cache_folder = cache_folder
-        self.cache = None
-        self.set_cache(cache_folder)
+def is_dir(path):
+    return os.path.isdir(path)
 
-    def _get_cached_files(self):
-        if not self.cache_folder:
-            return
-        folders = [f for f in os.listdir(self.cache_folder) if
-                   os.path.isdir(os.path.join(self.cache_folder, f))]
 
+class Cache(object):
+    def __init__(self, folder):
+        self.folder = folder
+        self.sites = {}
+        self._load_files()
+
+    def _load_files(self):
+        try:
+            os.makedirs(self.folder)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+
+        files = os.listdir(self.folder)
+        folders = [f for f in files if is_dir(os.path.join(self.folder, f))]
         cache = {}
         for f in folders:
-            path = os.path.join(self.cache_folder, f, "*.html")
+            path = os.path.join(self.folder, f, "*.html")
             cache[f] = {name: True for name in glob.glob(path)}
-        return cache
+        self.sites = cache
 
-    def set_cache(self, folder):
-        self.cache_folder = folder
-        self.cache = self._get_cached_files()
+    def exists(self, domain, filename):
+        if domain not in self.sites:
+            return False
+        site_cache = self.sites[domain]
+        long_filename = os.path.join(self.folder, domain, filename)
+        return long_filename in site_cache
 
-    def get_cached(self, url):
-        if not self.cache:
-            return
+    def get(self, url):
         domain, filename = url_info(url)
-        if self.is_cached(domain, filename):
+        if self.exists(domain, filename):
             print("<cache> {}".format(url))
-            long_filename = os.path.join(self.cache_folder, domain, filename)
+            long_filename = os.path.join(self.folder, domain, filename)
             with open(long_filename, "r") as fp:
                 return fp.read()
         return
 
-    def is_cached(self, domain, filename):
-        if domain not in self.cache:
-            return False
-        site_cache = self.cache[domain]
-        long_filename = os.path.join(self.cache_folder, domain, filename)
-        return long_filename in site_cache
+    def set(self, folder):
+        self.folder = folder
+        self._load_files()
 
-    def save_cached(self, url, text):
-        if not self.cache:
-            return
+    def save(self, url, text):
         domain, filename = url_info(url)
-        domain_folder = os.path.join(self.cache_folder, domain)
-        if domain not in self.cache:
-            self.cache[domain] = {filename: True}
+        domain_folder = os.path.join(self.folder, domain)
+        if domain not in self.sites:
+            self.sites[domain] = {filename: True}
             # make the folder
             os.makedirs(domain_folder)
         with open(os.path.join(domain_folder, filename), "w") as fp:
             fp.write(text)
+
+
+class Fetch(object):
+    def __init__(self, sleep_time=5, cache=None):
+        self.last = None
+        self.sleep_time = sleep_time
+        self.cache = cache
+
+    def set_cache(self, folder):
+        self.cache = Cache(folder)
+
+    def get_cached(self, url):
+        if not self.cache:
+            return
+        return self.cache.get(url)
+
+    def save_cached(self, url, text):
+        if not self.cache:
+            return
+        self.cache.save(url, text)
 
     def get(self, url):
         """
