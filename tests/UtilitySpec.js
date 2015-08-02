b955d02bb386d2146bf7373a55e23ee8fad2319c
Paul Sherman <paul.sherman.88@gmail.com>
2015-08-01 22:12:57 -0500

    remove old unit tests

--- tests/UtilitySpec.js
+++ tests/UtilitySpec.js
@@ -16,80 +16,33 @@ describe("utility functions", function(){
         });
     });
 
-    describe("addClass", function(){
-        it ("adds class to each element in an array", function(){
-            var eles = [],
-                curr;
-            for ( var i=0; i<20; i++ ){
-                curr = document.createElement("div");
-                eles.push(curr);
-            }
-            addClass("test", eles);
-            for ( var i=0; i<20; i++ ){
-                expect(eles[i].classList.contains("test")).toBe(true);
-            }
+    describe("abbreviate", function(){
+        it("returns text when length is less than max", function(){
+            var text = "characters";
+            expect(abbreviate(text, text.length)).toEqual(text);
         });
 
-        it ("adds class to each element in a node list", function(){
-            var holder = document.createElement("div"),
-                curr;
-            for ( var i=0; i<20; i++ ){
-                curr = document.createElement("div");
-                holder.appendChild(curr);
-            }
-            var eles = holder.getElementsByTagName("div");
-            addClass("test", eles);
-            for ( var i=0; i<20; i++ ){
-                expect(eles[i].classList.contains("test")).toBe(true);
-            }
-        });
-    })
-
-    describe("legalPageName", function(){
-        it("returns true for legal filenames", function(){
-            var goodNames = ["test", "good.jpg", "this is legal !"];
-            for ( var i=0, len=goodNames.length; i<len; i++ ) {
-                expect(legalPageName(goodNames[i])).toBe(true);
-            }
+        it("returns ellipsis when max <= 3", function(){
+            [0,1,2,3].forEach(function(val){
+                expect(abbreviate("test", val)).toEqual("...");
+            });
         });
 
-        it("returns false for filenames that contain illegal characters", function(){
-            var badNames = ["<", ">", ":", "\"", "\"", "/", "|", "?", "*"];
-            for ( var i=0, len=badNames.length; i<len; i++ ) {
-                expect(legalPageName(badNames[i])).toBe(false);
-            }
+        it("returns even first and second half length when max is odd", function(){
+            var abbr = abbreviate("a string of characters", 15);
+            var halves = abbr.split("...");
+            expect(halves[0].length).toEqual(6);
+            expect(halves[1].length).toEqual(6);
         });
 
-        it("returns false for null name", function(){
-            expect(legalPageName(null)).toBe(false);
+        it("returns longer first half when max is even", function(){
+            var abbr = abbreviate("a string of characters", 14);
+            var halves = abbr.split("...");
+            expect(halves[0].length).toEqual(6);
+            expect(halves[1].length).toEqual(5);
         });
     });
+
 
 });
 
-describe("abbreviate", function(){
-    it("returns text when length is less than max", function(){
-        var text = "characters";
-        expect(abbreviate(text, text.length)).toEqual(text);
-    });
-
-    it("returns ellipsis when max <= 3", function(){
-        [0,1,2,3].forEach(function(val){
-            expect(abbreviate("test", val)).toEqual("...");
-        });
-    });
-
-    it("returns even first and second half length when max is odd", function(){
-        var abbr = abbreviate("a string of characters", 15);
-        var halves = abbr.split("...");
-        expect(halves[0].length).toEqual(6);
-        expect(halves[1].length).toEqual(6);
-    });
-
-    it("returns longer first half when max is even", function(){
-        var abbr = abbreviate("a string of characters", 14);
-        var halves = abbr.split("...");
-        expect(halves[0].length).toEqual(6);
-        expect(halves[1].length).toEqual(5);
-    });
-});
