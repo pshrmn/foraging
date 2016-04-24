e685b82fe7fa90ddb476a30cd1cca3571f62c01a
Paul Sherman <paul.sherman.88@gmail.com>
2016-04-23 22:56:11 -0500

    stateless Frames component

--- src/components/frames/Frames.jsx
+++ src/components/frames/Frames.jsx
@@ -13,37 +13,30 @@ import ElementWizard from "./ElementWizard";
  * are a number of different frames associated with different states of viewing
  * and creating elements.
  *
- * Through its props, each frame is given any pertinent actions that, relevant
- * parts of the current element (referred to as the parent when creating a 
- * new element), and the destructured parts of a data object containing any
- * extra data for that frame.
  */
-const Frames = React.createClass({
-  _selectFrame: function() {
-    const { frame } = this.props;
-    /*
-     * only the necessary actions are sent to the frame components
-     */
-    switch ( frame.name ) {
-    case "element":
-      return <ElementFrame />
-    case "wizard":
-      return <ElementWizard />
-    case "rule":
-      return <RuleFrame />
-    default:
-      return null;
-    }
-  },
-
-  render: function() {
-    return (
-      <div className="frames">
-        {this._selectFrame()}
-      </div>
-    );
-  },
-});
+ function Frames(props) {
+  const { frame } = props;
+  /*
+   * only the necessary actions are sent to the frame components
+   */
+  let frameElement = null;
+  switch ( frame.name ) {
+  case "element":
+    frameElement = <ElementFrame />;
+    break;
+  case "wizard":
+    frameElement = <ElementWizard />;
+    break;
+  case "rule":
+    frameElement = <RuleFrame />;
+    break;
+  }
+  return (
+    <div className="frames">
+      { frameElement }
+    </div>
+  );
+}
 
 export default connect(
   state => ({
