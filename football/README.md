e8745d6130ec09f01edbc52e37a6eb55f6997da1
Paul Sherman <paul.sherman.88@gmail.com>
2016-08-25 14:34:50 -0500

    update football/schedule.py to be season agnostic

--- football/README.md
+++ football/README.md
@@ -1,3 +1,25 @@
 Basic NFL stadium info
 
+```
     python -m info
+```
+
+Get the coordinates of each NFL stadium
+
+```
+    python -m coordinates
+```
+
+Get weekly schedules for a given year. Specify the year using either the `-year` or `-Y` argument.
+
+```
+    python -m schedule -Y <year>
+```
+
+For each game within a week, a dict will be created with `home` and `away` keys. These will have the name of the home and away team for a given game. All of the games for a given week will be placed in an array. In the JSON output for a given week, the object will have a `games` key whose value is the array of games. If there are teams on a bye week for a given week, the JSON object will also have a `byes` key whose value is an array of team objects. Each team object has one key, `team`, whose value is the name of the team.
+
+You can also get only a specific week's schedule by providing the week using either the `-week` or `-W` argument.
+
+```
+    python -m schedule -Y 2016 -W 7
+```
--- football/schedule.py
+++ football/schedule.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 from gatherer import Page, Fetch, Cache
 
@@ -10,10 +11,35 @@ f = Fetch(headers={'User-Agent': 'gatherer agent'}, cache=c)
 
 schedule = Page.from_json(schedule_json)
 
-BASE_URL = "http://www.nfl.com/schedules/2015/REG{}"
 
-for week in range(1, 18):
-    dom = f.get(BASE_URL.format(week))
+def get_season(year):
+    BASE_URL = "http://www.nfl.com/schedules/{}/REG{}"
+
+    for week in range(1, 18):
+        dom = f.get(BASE_URL.format(year, week))
+        s = schedule.gather(dom)
+        with open("data/{}-{:02d}.json".format(year, week), "w") as fp:
+            json.dump(s, fp, indent=2)
+
+
+def get_week(year, week):
+    BASE_URL = "http://www.nfl.com/schedules/{}/REG{}"
+    dom = f.get(BASE_URL.format(year, week))
     s = schedule.gather(dom)
-    with open("data/week{}.json".format(week), "w") as fp:
+    with open("data/{}-{:02d}.json".format(year, week), "w") as fp:
         json.dump(s, fp, indent=2)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-year', '-Y', dest='year',
+                        help='year to get schedules for')
+    parser.add_argument('-week', '-W', dest='week',
+                        help='week to get schedule for')
+    args = parser.parse_args()
+    year = args.year
+    week = args.week
+    print(year, week)
+    if week is not None:
+        get_week(year, int(week))
+    else:
+        get_season(year)
